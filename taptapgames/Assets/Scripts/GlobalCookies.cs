765123787bcdd84b423fb9b5fca7475830de0d08
ReinerJasin <reinerjasin@gmail.com>
2021-07-24 00:22:10 +0800

    Episode 5 - 00:00

---
+++ taptapgames/Assets/Scripts/GlobalCookies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalCookies : MonoBehaviour
+{
+
+    public static int CookieCount;
+    public GameObject CookieDisplay;
+    public int InternalCookie;
+
+    // Start is called before the first frame update
+    /*
+    void Start()
+    {
+        
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalCookie = CookieCount;
+        CookieDisplay.GetComponent<Text>().text = "Cookies: " + InternalCookie;
+    }
+
+}
