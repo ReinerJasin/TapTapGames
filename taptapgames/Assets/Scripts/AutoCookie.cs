346cba3963bdef647dbdd4850c78923257a9fc4f
ReinerJasin <reinerjasin@gmail.com>
2021-07-24 15:53:00 +0800

    Episode 7 - 00:00

---
+++ taptapgames/Assets/Scripts/AutoCookie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCookie : MonoBehaviour
+{
+
+    public bool CreatingCookie = false;
+    public static int CookieIncrease = 1;
+    public int InternalIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalIncrease = CookieIncrease;
+        if (CreatingCookie == false)
+        {
+            CreatingCookie = true;
+            StartCoroutine(CreateTheCookie());
+        }
+    }
+
+    IEnumerator CreateTheCookie()
+    {
+        GlobalCookies.CookieCount += InternalIncrease;
+        yield return new WaitForSeconds(1);
+        CreatingCookie = false;
+    }
+}
---
+++ taptapgames/Assets/Scripts/PurchaseLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseLog : MonoBehaviour
+{
+    public GameObject AutoCookie;
+
+    public void StartAutoCookie()
+    {
+        AutoCookie.SetActive(true);
+        GlobalCash.CashCount -= GlobalBaker.bakerValue;
+        GlobalBaker.bakerValue *= 2;
+        GlobalBaker.turnOffButton = true;
+    }
+}
